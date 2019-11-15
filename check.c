3b28afd9f31421af7c3358b56988416f4fe0e3fe
Chiramium <chirami.industries@gmail.com>
2019-11-15 13:59:32 +0900

    [Add] "check.c" can be executed with own library.

---
+++ check.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mulprec.h"
+
+//
+//  Program Start
+//
+int main(int argc, char **argv)
+{
+    struct NUMBER a, b, c, d, e;
+    int r;
+
+    srandom(time(NULL));    //  乱数シードの初期化
+
+    clearByZero(&a);    //  a <= 0
+    clearByZero(&b);    //  b <= 0
+    clearByZero(&c);    //  c <= 0
+    clearByZero(&d);    //  d <= 0
+    clearByZero(&e);    //  e <= 0
+
+    printf("a \t= ");     //  0が表示されるか確認
+    dispNumber(&a);
+    printf("\n");
+
+    setRnd(&b, 10);     //  b <= 10桁の乱数(bの初期化はsetRnd()内で行うとする)
+    printf("b \t= ");     //  bを表示
+    dispNumber(&b);
+    printf("\n");
+
+    copyNumber(&b, &c); //  c <= b
+    printf("c \t= ");
+    dispNumber(&c);
+    printf("\n");
+
+    r = mulBy10(&b, &d);    //  d <= b * 10
+    printf("b * 10 \t= ");    //  dを表示
+    dispNumber(&d);
+    printf(", r = %d\n", r);
+
+    r = divBy10(&c, &e);    //  e <= c / 10
+    printf("c / 10 \t= ");    //  eを表示
+    dispNumber(&e);
+    printf(", r = %d\n", r);
+
+    printf("a == 0? --> %d\n", isZero(&a)); //  isZero()の結果を表示
+    printf("b == 0? --> %d\n", isZero(&b));
+    printf("c == 0? --> %d\n", isZero(&c));
+    printf("d == 0? --> %d\n", isZero(&d));
+
+    return 0;
+}
---
+++ mulprec.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define KETA 30
+
+struct NUMBER
+{
+	int n[KETA];	//	各桁の値
+	int sign;		//	符号変数
+};
+
+//	Prototype
+void clearByZero(struct NUMBER* a);
+void dispNumber(struct NUMBER* a);
+void setRnd(struct NUMBER *a, int k);
+void copyNumber(struct NUMBER *a, struct NUMBER *b);
+void getAbs(struct NUMBER *a, struct NUMBER *b);
+int isZero(struct NUMBER *a);
+int mulBy10(struct NUMBER *a, struct NUMBER *b);
+int divBy10(struct NUMBER *a, struct NUMBER *b);
